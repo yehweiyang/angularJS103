34201ff2d7efc79952287b02cc15a8a5d90f5e6a
weiyang <tw00182691@gmail.com>
2017-01-13 00:17:21 +0800

    edit css

--- WebContent/js/controller/MyFile1Controller.js
+++ WebContent/js/controller/MyFile1Controller.js
@@ -1,24 +1,42 @@
-﻿myApp.controller('MyFile1Controller', [ '$scope', '$rootScope', '$http','$filter',
-		function($scope, rootScope, $http,$filter) {
-	
+﻿myApp.controller('MyFile1Controller', [
+		'$scope',
+		'$rootScope',
+		'$http',
+		'$filter',
+		function($scope, rootScope, $http, $filter) {
+			// a sample array use for paging
+			$scope.list = [];
+
+			$scope.currentPage = 1; // keeps track of the current page
+			$scope.pageSize = 10 // holds the number of items per page
+
 			var json = {};
 
+			$scope.sortType = 'CUST_ID'; // set the default sort type
+			$scope.sortReverse = false; // set the default sort order
+			$scope.searchFish = ''; // set the default search/filter
+			// term
+
 			$scope.query = function() {
-				if($scope.userDate==undefined||$scope.userDate==null||$scope.userDate==''){
-					$scope.userDate='';
-				}else{
-					var bdata = $filter('date')(new Date($scope.userDate),'yyyy-MM-dd');
-					$scope.userDate=bdata;
+				if ($scope.userDate == undefined || $scope.userDate == null
+						|| $scope.userDate == '') {
+					$scope.userDate = '';
+				} else {
+					var bdata = $filter('date')(new Date($scope.userDate),
+							'yyyy-MM-dd');
+					$scope.userDate = bdata;
 				}
-				if($scope.userLv===undefined){
-					$scope.userLv='';
+				if ($scope.userLv === undefined) {
+					$scope.userLv = '';
 				}
-				$http.get("json/client.json").success(function(response) {
-					json = response;
-					$scope.status = true;
-					queryMethid(json,$scope.userNumber,$scope.userName,$scope.userLv,$scope.userDate);
-				});
-	
+				$http.get("json/client.json").success(
+						function(response) {
+							json = response;
+							$scope.status = true;
+							queryMethid(json, $scope.userNumber,
+									$scope.userName, $scope.userLv,
+									$scope.userDate);
+						});
 
 			};
 
@@ -30,34 +48,34 @@
 				$scope.userLv = '';
 			};
 
-			var queryMethid=function (data,number,name,level,enterDate){
+			var queryMethid = function(data, number, name, level, enterDate) {
 				var filter = {};
 				if (number != '') {
-					filter.CUST_ID=$scope.userNumber;
+					filter.CUST_ID = $scope.userNumber;
 				}
 				if (name != '') {
-					filter.CUST_NAME=$scope.userName;
+					filter.CUST_NAME = $scope.userName;
 				}
 				if (level != '') {
-					filter.VIP_CODE=$scope.userLv;
+					filter.VIP_CODE = $scope.userLv;
 				}
 				if (enterDate != '') {
-					filter.ENTRY_DATE=$scope.userDate;
+					filter.ENTRY_DATE = $scope.userDate;
 				}
-				data = data.filter(function(item) {
+				data = data
+						.filter(function(item) {
 
-				    for(var key in filter) {
+							for ( var key in filter) {
 
-				        if(item[key] === undefined || item[key] != filter[key])
-				            return false;
+								if (item[key] === undefined
+										|| item[key] != filter[key])
+									return false;
 
-				    }
-				    return true;
-				});
-				$scope.datas =data;
-		
-
+							}
+							return true;
+						});
+				$scope.datas = data;
+				$scope.total = data.length;
 			};
-
 
 		} ]);
--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,60 +1,35 @@
-﻿var myApp = angular.module("myApp", []);
+﻿var myApp = angular.module("myApp", [ 'ui.bootstrap' ]).filter('start',
+		function() {
+			return function(input, start) {
+				if (!input || !input.length) {
+					return;
+				}
 
-myApp.controller('AppController', [ '$scope', '$rootScope', function($scope) {
+				start = +start;
+				return input.slice(start);
+			};
+		}).filter('lv', function() {
+	return function(number) {
+		if (number === 'N') {
+			return '一般';
+		} else if (number === 'G') {
+			return '黃金';
+		} else if (number === 'D') {
+			return '鑽石';
+		}
 
-	$scope.reset = function() {
-		console.log($scope.userName);
-		console.log($scope.userNumber);
-		$scope.userNumber = '';
-		$scope.userName = '';
-		alert('clear');
-		console.log('reset.......');
-	}
-	$scope.send = function() {
-		alert('查詢條件 : ' + $scope.userNumber + ' ' + $scope.userName);
 	};
-
-} ]);
-
-myApp.controller('AppController2', [ '$scope', '$http',
-		function($scope, $http) {
-			var myObj = [ {
-				"number" : '001',
-				"name" : '吉米',
-				"deposit" : 998
-			}, {
-				"number" : '002',
-				"name" : '肯',
-				"deposit" : 780000
-			}, {
-				"number" : '003',
-				"name" : '大衛',
-				"deposit" : 200000
-			}, {
-				"number" : '004',
-				"name" : '亞瑟',
-				"deposit" : 900000
-			}, {
-				"number" : '005',
-				"name" : '艾倫',
-				"deposit" : 5566
-			}, {
-				"number" : '006',
-				"name" : '尚',
-				"deposit" : 100000
-			} ];
-			$scope.myObj = myObj;
-
-		} ]);
+});
+;
 
 myApp.controller('AppController3', [ '$scope', '$http',
 		function($scope, $http) {
-			
+
 			$scope.change = function() {
 				$http.get('json/level.json').success(function(data) {
 					// you can do some processing here
 					$scope.userLevel = data;
 				});
-			}
+			};
 
 		} ]);
